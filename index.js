f3d6f7f6bc51bc879e08cbf9d0abd9395640a97a
Mikej81 <stumpj@gmail.com>
2017-05-01 15:28:18 -0700

    initial

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ var shell = require('shelljs');
 
 var selects = [];
 var simpleselect = {};
+var simpleselect2 = {};
 
-simpleselect.query = '#alert';
+simpleselect.query = 'head';
 simpleselect.func = function (node) {
-	var out = '<script type="text/javascript">function coleman() {alert("oh no!");}</script>';
+	var out = '<script type="text/javascript">alert("hello")</script>';
 
 	var rs = node.createReadStream();
 	var ws = node.createWriteStream({outer:false});
@@ -24,8 +25,15 @@ simpleselect.func = function (node) {
 	
 }
 
+simpleselect2.query = '#alert';
+simpleselect2.func = function (node) {
+	node.createWriteStream().end('<div id="alert" onLoad="$(this).text(\'UserLoginStats\')">');
+}
+
 
 //selects.push(simpleselect);
+selects.push(simpleselect2);
+
 
 var app = connect();
 
@@ -96,9 +104,10 @@ function getUserStats(username) {
 var loginstats = '<script> function loginStats() { alert('+ username +');}</script>';
 
 guiProxy.on('proxyRes', function(proxyRes, request, response) {
+	var notified = proxyRes.headers['user-notified'];
 	var cookies = proxyRes.headers['set-cookie'];
 	if (proxyRes.headers['content-type'] == 'text/html') {
-		console.log("before: " + JSON.stringify(proxyRes.headers, true, 2));
+		//console.log("before: " + JSON.stringify(proxyRes.headers, true, 2));
 	}
 	//console.log("before: " + JSON.stringify(proxyRes.headers, true, 2));
 	if (cookies) {
@@ -106,11 +115,12 @@ guiProxy.on('proxyRes', function(proxyRes, request, response) {
 		var cookiestring = cookies.toString().substr(usearch).replace(usernameCookie + "=", '');
 		var userloginstats = getUserStats(cookiestring);
 		//console.log(userloginstats);
+		//console.log('not alerted! pull tmsh and alert script');
 	}
 	if (proxyRes.headers['content-type'] == 'text/html' && proxyRes.headers['content-length']) {
 		delete proxyRes.headers['content-length']
 	}
-	console.log("after: " + JSON.stringify(proxyRes.headers, true, 2));        
+//	console.log("after: " + JSON.stringify(proxyRes.headers, true, 2));        
 });
 
 guiProxy.on('error', function (err, req, res) {
