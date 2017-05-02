629c27a811a85137cf8e2f7294f64e79fa674734
Mikej81 <stumpj@gmail.com>
2017-05-01 23:15:31 -0700

    initial

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 var http = require('http'),
 	connect = require('connect'),
         https = require('https');
@@ -9,11 +11,14 @@ var shell = require('shelljs');
 
 var selects = [];
 var simpleselect = {};
-var simpleselect2 = {};
 
-simpleselect.query = 'head';
+simpleselect.query = 'document';
 simpleselect.func = function (node) {
-	var out = '<script type="text/javascript">alert("hello")</script>';
+	var out = '<loginstats label="Failed Logons since Last" />';
+//	var out = '<div id="myAlert" style="display:none" class="alert alert-warning alert-dismissible" role="alert">';
+//	out +=    '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
+//      out +=	  '<span aria-hidden="true">&times;</span> </button>';
+//	out +=    'Logon Stats</div>';
 
 	var rs = node.createReadStream();
 	var ws = node.createWriteStream({outer:false});
@@ -25,17 +30,11 @@ simpleselect.func = function (node) {
 	
 }
 
-simpleselect2.query = '#alert';
-simpleselect2.func = function (node) {
-	node.createWriteStream().end('<div id="alert" onLoad="$(this).text(\'UserLoginStats\')">');
-}
-
-
-//selects.push(simpleselect);
-selects.push(simpleselect2);
-
 
 var app = connect();
+
+// Replacement XUI.JS File
+var xuiJS = fs.readFileSync("./xui.js", "utf8");
 
 // BIG-IP Paths
 bigKey = "/config/httpd/conf/ssl.key/server.key";
@@ -75,52 +74,63 @@ var guiProxy = httpProxy.createProxyServer({
 
 app.use(require('harmon')([], selects));
 app.use(function (req, res) {
-	guiProxy.web(req, res, {target: 'https://127.0.0.1:444'});
+		guiProxy.web(req, res, {target: 'https://127.0.0.1:444'});
 });
 
-https.createServer(httpsCreds, app, function(req, res) {
-	console.log("req");
-	console.log("res");
-
-}).listen(443);
+https.createServer(httpsCreds, app, function(req, res) {}).listen(443);
 
 //We need to get username from logon, so look for the BIGIPAuthUsernameCookie
-var usernameCookie = "BIGIPAuthUsernameCookie";
+	var usernameCookie = "BIGIPAuthUsernameCookie";
 //BIGIPAuthCookie=0BD6095556437BA7A5D7F9A786362FA5A690193E',
 //BIGIPAuthUsernameCookie=xadmin'
-var username;
+	var username;
 
 var proxyRequest = guiProxy.on('proxyReq', function(proxyReq, req, res, options) {
-//	console.log(proxyReq);	
+	//	console.log(proxyReq);	
 });
 
 //Section for Login Details Popup
 function getUserStats(username) {
 	var userstatscmd = shell.exec('tmsh show auth login ' + username, {silent:true}).stdout;
-
 	return userstatscmd;
 };
 
-var loginstats = '<script> function loginStats() { alert('+ username +');}</script>';
-
-guiProxy.on('proxyRes', function(proxyRes, request, response) {
-	var notified = proxyRes.headers['user-notified'];
+var proxyResponse = guiProxy.on('proxyRes', function(proxyRes, request, response) {
 	var cookies = proxyRes.headers['set-cookie'];
-	if (proxyRes.headers['content-type'] == 'text/html') {
-		//console.log("before: " + JSON.stringify(proxyRes.headers, true, 2));
-	}
-	//console.log("before: " + JSON.stringify(proxyRes.headers, true, 2));
 	if (cookies) {
-		var usearch = cookies.toString().search(usernameCookie);
-		var cookiestring = cookies.toString().substr(usearch).replace(usernameCookie + "=", '');
-		var userloginstats = getUserStats(cookiestring);
-		//console.log(userloginstats);
-		//console.log('not alerted! pull tmsh and alert script');
+                var usearch = cookies.toString().search(usernameCookie);
+                var cookiestring = cookies.toString().substr(usearch).replace(usernameCookie + "=", '');
+		var uName = cookiestring.substr(0, cookiestring.indexOf(';'));
+                var userloginstats = getUserStats(uName);
+        }
+	
+	var _write = response.write;
+	var output = "";
+	var body = "";
+	var newbody = "";
+
+	proxyRes.on('data', function(data) {
+		if (proxyRes.headers['content-type'] === 'text/xml'){
+                	data = data.toString('utf-8');
+                	body += data;
+			//var newbody = body.replace('</document>',"<loginstats label=\"" + userloginstats+ "\" user=\"" + uName + "\" />\r\n</document>");
+			newbody = body.replace('</document>',"<loginstats label=\"userloginstats\" user=\"" + uName + "\" />\r\n</document>");
+			//console.log(newbody);
+		} else if (proxyRes.headers['content-type'] === 'text/javascript' && request.url.indexOf('xui.js') > -1) {
+			data = data.toString('utf-8');
+			body += data;
+			newbody = xuiJS;
+		}
+	});
+	proxyRes.write = function(data) {
+		try {
+	  		_write.call(response,newbody);
+		} catch (err){}
 	}
+
 	if (proxyRes.headers['content-type'] == 'text/html' && proxyRes.headers['content-length']) {
 		delete proxyRes.headers['content-length']
 	}
-//	console.log("after: " + JSON.stringify(proxyRes.headers, true, 2));        
 });
 
 guiProxy.on('error', function (err, req, res) {
